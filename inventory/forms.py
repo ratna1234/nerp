34c948d873ce1fafbabf3f93877b7ab917c7cb31
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-21 16:49:09 +0545

    created basic inventory form

---
+++ inventory/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class CategoryForm(forms.Form):
+    code = forms.CharField(max_length=10, required=False)
+    name = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
+
+
+class ItemForm(forms.Form):
+    code = forms.CharField(max_length=10, required=False)
+    name = forms.CharField(max_length=100)
+    price = forms.FloatField(required=False)
+    description = forms.CharField(widget=forms.Textarea)
