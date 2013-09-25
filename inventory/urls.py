68772779f0a682bb6f2c4df61ce70898263e8a0a
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-25 18:24:44 +0545

    template for purchase order

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
                        url(r'^create/$', views.item_form, name='create_inventory_item'),
                        url(r'^item/delete/(?P<id>[0-9]+)$', views.delete_inventory_item, name='delete_inventory_item'),
                        url(r'^(?P<id>[0-9]+)/$', views.item_form, name='update_inventory_item'),
-                       url(r'^items/json/$', views.items_as_json, name='items_as_json'),
+                       url(r'^items.json$', views.items_as_json, name='items_as_json'),
 
                        url(r'^categories/$', views.list_categories, name='list_inventory_categories'),
                        url(r'^category/create/$', views.create_category, name='create_inventory_category'),
