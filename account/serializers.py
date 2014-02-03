ec7095df2b25079e74e215354a0ed828330afad5
Dipesh Acharya <xtranophilist@gmail.com>
2014-02-03 17:56:04 +0545

    fixed serializers for MultiNameModels to include name

--- account/serializers.py
+++ account/serializers.py
@@ -1,12 +1,22 @@
 from rest_framework import serializers
-from account.models import Receipt
+from account.models import Receipt, ReceiptRow
 from core.models import Account
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='name')
+
     class Meta:
         model = Account
 
+
+class ReceiptRowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReceiptRow
+
+
 class ReceiptSerializer(serializers.ModelSerializer):
+    rows = ReceiptRowSerializer()
+
     class Meta:
         model = Receipt
--- core/serializers.py
+++ core/serializers.py
@@ -3,25 +3,35 @@ from core.models import Party, Employee, Donor, Activity, Budget
 
 
 class PartySerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='name')
+
     class Meta:
         model = Party
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='name')
+
     class Meta:
         model = Employee
 
 
 class DonorSerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='name')
+
     class Meta:
         model = Donor
 
 
 class ActivitySerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='name')
+
     class Meta:
         model = Activity
 
 
 class BudgetSerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='name')
+
     class Meta:
         model = Budget
