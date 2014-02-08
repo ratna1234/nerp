be04b12a277f0cdb9c952987b2a23ae63e6d9981
Dipesh Acharya <xtranophilist@gmail.com>
2014-02-08 19:04:06 +0545

    implemented saving of receipt

--- account/urls.py
+++ account/urls.py
@@ -5,8 +5,9 @@ urlpatterns = patterns('',
 
                        # url(r'^receipts/$', views.list_receipts, name='list_receipts'),
                        url(r'^receipt/create/$', views.receipt, name='create_receipt'),
-                       url(r'^receipt/(?P<id>[0-9]+)/$', views.receipt, name='update_receipt'),
+                       url(r'^receipt/(?P<pk>[0-9]+)/$', views.receipt, name='update_receipt'),
                        # url(r'^receipt/(?P<id>[0-9]+)/delete/$', views.delete_receipt, name='delete_receipt'),
+                       url(r'^receipt/save/$', views.save_receipt, name='save_receipt'),
 
 
 
--- account/views.py
+++ account/views.py
@@ -1,16 +1,62 @@
 from datetime import date
+import json
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from account.models import Receipt, ReceiptRow
 from account.serializers import ReceiptSerializer
+from app.libr import save_model, invalid
+from inventory.models import delete_rows
 
 
 def receipt(request, pk=None):
     if pk:
-        obj = get_object_or_404(Receipt, id=id)
+        obj = get_object_or_404(Receipt, pk=pk)
         scenario = 'Update'
     else:
         obj = Receipt(date=date.today())
         scenario = 'New'
     data = ReceiptSerializer(obj).data
     return render(request, 'receipt.html', {'scenario': scenario, 'data': data})
+
+
+def save_receipt(request):
+    params = json.loads(request.body)
+    dct = {'rows': {}}
+    object_values = {'no': params.get('no'), 'fiscal_year': request.setting.fiscal_year,
+                     'date': params.get('date')}
+    if params.get('id'):
+        obj = Receipt.objects.get(id=params.get('id'))
+    else:
+        obj = Receipt()
+    try:
+        obj = save_model(obj, object_values)
+    except Exception as e:
+        if hasattr(e, 'messages'):
+            dct['error_message'] = '; '.join(e.messages)
+        elif str(e) != '':
+            dct['error_message'] = str(e)
+        else:
+            dct['error_message'] = 'Error in form data!'
+    dct['id'] = obj.id
+    model = ReceiptRow
+    for index, row in enumerate(params.get('table_view').get('rows')):
+        if invalid(row, ['budget_head_id', 'account_id', 'tax_scheme_id']):
+            continue
+        values = {'sn': index + 1, 'budget_head_id': row.get('budget_head_id'),
+                  'account_id': row.get('account_id'), 'invoice_no': row.get('invoice_no'),
+                  'nepal_government': row.get('nepal_government'), 'foreign_cash_grant': row.get('foreign_cash_grant'),
+                  'foreign_compensating_grant': row.get('foreign_compensating_grant'),
+                  'foreign_cash_loan': row.get('foreign_cash_loan'),
+                  'foreign_compensating_loan': row.get('foreign_compensating_loan'),
+                  'foreign_substantial_aid': row.get('foreign_substantial_aid'),
+                  'advanced': row.get('advanced'), 'cash_returned': row.get('cash_returned'),
+                  'advanced_settlement': row.get('advanced_settlement'), 'vattable': row.get('vattable'),
+                  'tax_scheme_id': row.get('tax_scheme_id'), 'activity_id': row.get('activity_id'),
+                  'remarks': row.get('remarks'), 'receipt': obj}
+        submodel, created = model.objects.get_or_create(id=row.get('id'), defaults=values)
+        if not created:
+            submodel = save_model(submodel, values)
+        dct['rows'][index] = submodel.id
+    delete_rows(params.get('table_view').get('deleted_rows'), model)
+    return HttpResponse(json.dumps(dct), mimetype="application/json")
