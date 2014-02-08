80959d44817434ae0b2e3e9e9a8a017f78f39b68
Dipesh Acharya <xtranophilist@gmail.com>
2014-02-08 19:03:44 +0545

    fixed receipt row serialzier

--- account/serializers.py
+++ account/serializers.py
@@ -11,8 +11,14 @@ class AccountSerializer(serializers.ModelSerializer):
 
 
 class ReceiptRowSerializer(serializers.ModelSerializer):
+    tax_scheme_id = serializers.Field(source='tax_scheme.id')
+    budget_head_id = serializers.Field(source='budget_head.id')
+    account_id = serializers.Field(source='account.id')
+    activity_id = serializers.Field(source='activity.id')
+
     class Meta:
         model = ReceiptRow
+        exclude = ['tax_scheme', 'budget_head', 'account', 'activity']
 
 
 class ReceiptSerializer(serializers.ModelSerializer):
