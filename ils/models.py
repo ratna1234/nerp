141ec95487690f5dd9c344d0471f1eb7b8ac62d0
Dipesh Acharya <xtranophilist@gmail.com>
2014-03-24 19:20:34 +0545

    basic db design

--- ils/models.py
+++ ils/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from django.conf.global_settings import LANGUAGES
+from app.libr import unique_slugify
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=254, null=True, blank=True)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children')
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        unique_slugify(self, self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = u'Book Categories'
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        unique_slugify(self, self.name)
+        super(Author, self).save(*args, **kwargs)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=254)
+    author = models.ForeignKey(Author)
+    language = models.CharField(max_length=7, choices=LANGUAGES, default='en')
+    category = models.ForeignKey(Category)
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        unique_slugify(self, self.title)
+        super(Book, self).save(*args, **kwargs)
+
+
+
