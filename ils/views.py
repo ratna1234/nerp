e1d5acf2f250e9234f5ca37a70365180dedcbafb
Dipesh Acharya <xtranophilist@gmail.com>
2014-03-29 17:11:12 +0545

    ils: retrieving of book data from openlibrary and savinjg it

--- ils/views.py
+++ ils/views.py
@@ -1,6 +1,98 @@
 from django.shortcuts import render
+import isbn as isbnpy
+import urllib2, urllib
+import json
+import pprint
+from models import Record, Author, Publisher, Book
+import os
+from django.core.files import File
+from datetime import datetime
+
+pp = pprint.PrettyPrinter(indent=4).pprint
 
 
 # Create your views here.
 def acquisition(request):
+    if request.GET.get('isbn'):
+        isbn = request.GET.get('isbn')
+        if isbnpy.isValid(isbn):
+            response = urllib2.urlopen('http://openlibrary.org/api/volumes/brief/json/isbn:' + isbn)
+            data = json.load(response)
+            data = data.itervalues().next()['records'].itervalues().next()
+            if isbnpy.isI10(isbn):
+                isbn = isbnpy.convert(isbn)
+            try:
+                record = Record.objects.get(isbn13=isbn)
+            except Record.DoesNotExist:
+                record = Record(isbn13=isbn)
+                # pp(data)
+            if record.book_id:
+                book = record.book
+            else:
+                book = Book()
+            book.title = data['data']['title']
+            book.save()
+            record.pagination = data['data']['number_of_pages']
+            record.format = data['details']['details']['physical_format']
+            record.openlibrary_url = data['data']['url']
+            record.date_of_publication = datetime.strptime(data['data']['publish_date'], '%B %d, %Y').date()
+            record.openlibrary_id = data['data']['identifiers']['openlibrary'][0]
+            record.goodreads_id = data['data']['identifiers']['goodreads'][0]
+            record.librarything_id = data['data']['identifiers']['librarything'][0]
+            record.book = book
+            record.date_added = datetime.today()
+            record.save()
+            record.book.authors.all().delete()
+            for author in data['details']['details']['authors']:
+                author_key = author['key'].replace('/authors/', '')
+                try:
+                    book_author = Author.objects.get(identifier=author_key)
+                except Author.DoesNotExist:
+                    book_author = Author(identifier=author_key)
+                book_author.name = author['name']
+                book_author.save()
+                record.book.authors.add(book_author)
+                # record.publishers.all().delete()
+            # for publisher in data['details']['details']['publishers']:
+            #     try:
+            #         book_publisher = Publisher.objects.get(name=publisher['name'])
+            #     except Publisher.DoesNotExist:
+            #         book_publisher = Publisher(name=publisher['name'])
+            #         book_publisher.save()
+            #     record.publishers.add(book_publisher)
+            # try:
+            #     book_publisher = Publisher.objects.get(name=data['details']['details']['publishers'][0]['name'])
+            # except Publisher.DoesNotExist:
+            #     book_publisher = Publisher(name=data['details']['details']['publishers'][0]['name'])
+            #     book_publisher.save()
+            # record.publisher = book_publisher
+
+            cover_url = data['data']['cover']['large']
+            result = urllib.urlretrieve(cover_url)
+            record.large_cover.save(
+                os.path.basename(cover_url),
+                File(open(result[0]))
+            )
+            cover_url = data['data']['cover']['medium']
+            result = urllib.urlretrieve(cover_url)
+            record.medium_cover.save(
+                os.path.basename(cover_url),
+                File(open(result[0]))
+            )
+            cover_url = data['data']['cover']['small']
+            result = urllib.urlretrieve(cover_url)
+            record.small_cover.save(
+                os.path.basename(cover_url),
+                File(open(result[0]))
+            )
+            thumbnail_url = data['details']['thumbnail_url']
+            result = urllib.urlretrieve(thumbnail_url)
+            record.small_cover.save(
+                os.path.basename(thumbnail_url),
+                File(open(result[0]))
+            )
+
+            import pdb
+
+            pdb.set_trace()
     return render(request, 'acquisition.html', {})
