11615e239d09e86d51c9a458c70a82eeed1ca853
Dipesh Acharya <xtranophilist@gmail.com>
2014-03-31 12:13:50 +0545

    Language in admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import FiscalYear, AppSetting, Donor, Activity, BudgetHead, Employee, Party, Account, TaxScheme, BudgetBalance
+from core.models import FiscalYear, AppSetting, Donor, Activity, BudgetHead, Employee, Party, Account, TaxScheme, BudgetBalance, Language
 
 admin.site.register(FiscalYear)
 admin.site.register(Donor)
@@ -10,6 +10,7 @@ admin.site.register(Employee)
 admin.site.register(Party)
 admin.site.register(Account)
 admin.site.register(TaxScheme)
+admin.site.register(Language)
 
 
 class SettingsAdmin(admin.ModelAdmin):
