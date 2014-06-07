df3beaf5e0d04f89e6308af08c9a88a948cf9fd8
Dipesh Acharya <xtranophilist@gmail.com>
2014-06-07 10:52:36 +0545

    form and serializer fixes

--- training/forms.py
+++ training/forms.py
@@ -6,23 +6,23 @@ from users.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
-class TrainingForm(KOModelForm):
+class TrainingForm(forms.ModelForm):
     #category = TreeNodeChoiceField(queryset=Category.objects.all(), required=False)
 
     class Meta:
         model = Training
 
 
-class CategoryForm(KOModelForm):
+class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
 
 
-class ResourcePersonForm(KOModelForm):
+class ResourcePersonForm(forms.ModelForm):
     class Meta:
         model = ResourcePerson
 
 
-class TargetGroupForm(KOModelForm):
+class TargetGroupForm(forms.ModelForm):
     class Meta:
         model = TargetGroup
--- training/serializers.py
+++ training/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from training.models import Participant
+from training.models import Participant, Organization
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
 
 
 class ParticipantSerializer(serializers.ModelSerializer):
+    organization = OrganizationSerializer()
+
     class Meta:
         model = Participant
+
+
