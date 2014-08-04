f03b9da1c44eae4c0ed0ea1111f28f0aaa491d51
Dipesh Acharya <xtranophilist@gmail.com>
2014-08-04 23:22:29 +0545

    added static files to master

--- app/settings.py
+++ app/settings.py
@@ -54,7 +54,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-
+    os.path.join(APP_ROOT, '..', 'static'),
 )
 
 # List of finder classes that know how to find static files in
